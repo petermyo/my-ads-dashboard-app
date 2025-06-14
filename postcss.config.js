b862fd3629a3d3d4954952119831f4e6a9765054
petermyo <p3t3rmyo@gmail.com>
2025-06-14 18:25:48 +0630

    css

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// postcss.config.js
+// This file configures PostCSS for Create React App to process Tailwind CSS.
+
+module.exports = {
+    plugins: {
+      tailwindcss: {}, // Include Tailwind CSS
+      autoprefixer: {}, // Include Autoprefixer for vendor prefixes
+    },
+  };
+  
